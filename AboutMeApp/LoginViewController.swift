19c8ffe41b2aeb7e3e210e4054a6b0e4e1b901c0
RCherezbiev <149434791+RCherezbiev@users.noreply.github.com>
2023-11-28 03:02:46 +0300

    Final app

--- AboutMeApp/LoginViewController.swift
+++ AboutMeApp/LoginViewController.swift
@@ -28,7 +28,10 @@ final class LoginViewController: UIViewController {
     
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
         guard userNameTextField.text == user, passwordTextField.text == password else {
-            showAlert(withTitle: "Invalid login or password", andMessage: "Please, enter correct login and password")
+            showAlert(
+                withTitle: "Invalid login or password",
+                andMessage: "Please, enter correct login and password"
+            )
             return false
         }
         return true
@@ -45,15 +48,26 @@ final class LoginViewController: UIViewController {
     }
     
     @IBAction func forgotUsernameAction() {
-        showAlert(withTitle: "Oops!", andMessage: "Your name is User! \u{1F609}")
+        showAlert(
+            withTitle: "Oops!",
+            andMessage: "Your name is User! \u{1F609}"
+        )
     }
     
     @IBAction func forgotPasswordAction() {
-        showAlert(withTitle: "Oops!", andMessage: "Your password is 123! \u{1F609}")
+        showAlert(
+            withTitle: "Oops!",
+            andMessage: "Your password is 123! \u{1F609}"
+        )
     }
     
     private func showAlert(withTitle title: String, andMessage message: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        
         let okAction = UIAlertAction(title: "OK", style: .default)
         alert.addAction(okAction)
         present(alert, animated: true)
